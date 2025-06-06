b75278f1aad933414ffc77e42516b0fd38d14d39
Fabricio Castro <fabriciocastro002@gmail.com>
2025-06-05 23:54:02 -0600

    Migracion a Wampserver

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema; // <- Importante
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // Solución al error de longitud de clave en MySQL
+        Schema::defaultStringLength(191);
     }
 }
